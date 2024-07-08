505abb42720f11e6edc405afbe502913063a3903
nikhilvijj <nikhilvijj@gmail.com>
2024-07-08 14:00:49 +0530

    updates2

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const {ApolloServer} = require('apollo-server-express');
+const mongoose = require('mongoose')
+const {ApolloServer,gql } = require('apollo-server-express');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+const cors = require('cors')//import cors
+const userApiFromRouter = require('./routes/userRoutes') //import
+const app = express() 
+const port = 3001;
+const url= 'mongodb+srv://nikhilvijj:manager@cluster0.btrxovb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
-const app = express();
-const port = 3001
-const url='mongodb+srv://nikhilvijj:manager@cluster0.btrxovb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-app.use(express.json())//parsing
-mongoose.connect(url).then(()=>{console.log('DB connected')})
-.catch((err)=>{console.log(err)});
-//start my apollo Express server 
+app.use(express.json())
+app.use(cors()) //using cors
+mongoose.connect(url,{useNewUrlParser:true,
+useUnifiedTopology:true})
+.then(()=>{console.log('DB connected')})
+.catch((err)=>{console.log(err)})
 const server = new ApolloServer({typeDefs,resolvers});
-async function  StartServer(){
-    await server.start();
-    server.applyMiddleware({app});//run my express code
-    app.listen(port,()=>{
-        console.log('server live 3001');
-    })
+app.use('/users',userApiFromRouter);//add router
+async function StartServer(){
+   await server.start();
+   server.applyMiddleware({app});
+   app.listen(port,()=>{
+    console.log('Server Live 3001');
+   })
 }
+function TESTING(){
+    return 1;
+}
+function Test2(){return false;}
+TESTING();
+Test2();
 StartServer();
